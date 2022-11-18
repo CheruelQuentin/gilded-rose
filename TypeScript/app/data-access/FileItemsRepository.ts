30b2a4ebdac755d3855ae8bbff23e7c78367778a
CheruelQuentin <65130218+CheruelQuentin@users.noreply.github.com>
2022-11-18 09:41:58 +0100

    FileItemRepository

---
+++ TypeScript/app/data-access/FileItemsRepository.ts
@@ -0,0 +1,19 @@
+import * as fs from "fs"
+import Item from "../item/Item"
+import ItemRepository from "../item/ItemRepository"
+
+class FileItemsRepository {
+  constructor(private path: string) {
+    this.path = path
+  }
+
+  loadItemsFromFiles(): ItemRepository {
+    const items: Item[] = []
+    const fileContents = fs.readFileSync(this.path, "utf8")
+    return new ItemRepository(JSON.parse(fileContents))
+  }
+
+  saveItemsToFile(items: ItemRepository): void {
+    fs.writeFileSync(this.path, JSON.stringify(items.getInventory()))
+  }
+}
